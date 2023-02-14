e5737721c87754b80234bfeae0763706e2d84b3c
rwik2000 <rwikrana@gmail.com>
2023-02-13 17:28:17 -0800

    hypothesis works, removing rnn, simple 2 Linear layers

--- model.py
+++ model.py
@@ -2,29 +2,38 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+import numpy as np
+
 class GainTune(nn.Module):
     def __init__(self, batch_size=1):
         super().__init__()
         self.flag = 1
         self.batch_size = batch_size
         # self.h = torch.randn(2, batch_size, 3)
-        self.rnn = nn.RNN(5, 3, 3, bidirectional=False, batch_first=True)
-        self.linear = nn.Linear(3, 1)
+        # self.rnn = nn.RNN(5, 3, 1, bidirectional=False, batch_first=True)
+        
+        self.linear1 = nn.Linear(5, 3)
+        self.linear2 = nn.Linear(3, 1)
+
 
     def forward(self, x):
-        # if self.flag:
-        #     h = self.h0
-        #     self.flag = 0
-        # print(h)
+
         x = x.float()
-        h = self.init_hidden()
+        # h = self.init_hidden()
 
-        x,h = self.rnn(x,h)
-        x = self.linear(x.squeeze(1))
-        return x,h 
+        # x,h = self.rnn(x,h)
+        x = self.linear1(x)
+        x = self.linear2(x)
+        return x
 
-    def init_hidden(self,):
-        # This method generates the first hidden state of zeros which we'll use in the forward pass
-        # We'll send the tensor holding the hidden state to the device we specified earlier as well
-        hidden = torch.zeros(3, self.batch_size, 3)
-        return hidden
+    # def init_hidden(self,):
+    #     # This method generates the first hidden state of zeros which we'll use in the forward pass
+    #     # We'll send the tensor holding the hidden state to the device we specified earlier as well
+    #     hidden = torch.zeros(1, self.batch_size, 3)
+    #     return hidden
+
+p = GainTune()
+
+a = torch.tensor(np.random.rand(5,4,5))
+
+op = p(a)
--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.ppo_data import ppo_dataset
 from model import GainTune
 import numpy as np
-from torch.utils.data import DataLoader
+from torch.utils.ppo_data import ppo_dataLoader
 
 
 import os
@@ -14,7 +14,7 @@ def rolling_window(a, window):
     strides = a.strides + (a.strides[-1],)
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
-class data(Dataset):
+class ppo_data(ppo_dataset):
     def __init__(self, files, batch_size = 10):
         self.files = files
 
@@ -24,19 +24,19 @@ class data(Dataset):
         ppo_ang      =np.empty((0,3))
         
         for i in range(len(files)):
-            saved_data = np.load(files[i])
+            saved_ppo_data = np.load(files[i])
 
-            ts = saved_data['ts']
+            ts = saved_ppo_data['ts']
 
             l = len(ts)
             rl = len(ts[ts>10.0])
 
             p = l - rl
             ts = ts[p:]
-            thrust_cmds =  np.concatenate((thrust_cmds, np.expand_dims(saved_data['thrust_cmds'][p:],1)),axis=0)
-            ang_vel_cmds = np.concatenate((ang_vel_cmds,saved_data['ang_vel_cmds'][p:]),axis=0)
-            ppo_thrust =   np.concatenate((ppo_thrust,np.expand_dims(saved_data['ppo_acc'][p:],1)),axis=0)
-            ppo_ang =      np.concatenate((ppo_ang, saved_data['ppo_ang'][p:]),axis=0)
+            thrust_cmds =  np.concatenate((thrust_cmds, np.expand_dims(saved_ppo_data['thrust_cmds'][p:],1)),axis=0)
+            ang_vel_cmds = np.concatenate((ang_vel_cmds,saved_ppo_data['ang_vel_cmds'][p:]),axis=0)
+            ppo_thrust =   np.concatenate((ppo_thrust,np.expand_dims(saved_ppo_data['ppo_acc'][p:],1)),axis=0)
+            ppo_ang =      np.concatenate((ppo_ang, saved_ppo_data['ppo_ang'][p:]),axis=0)
         
         # self.thrust_cmds = rolling_window(thrust_cmds,5)
         self.thrust_cmds = thrust_cmds[5:]
@@ -61,50 +61,118 @@ class data(Dataset):
         ppo = self.ppo[idx]
         return dict(pid=pid,ppo=ppo)
 
-files = ["Data/log_12_02_20.npz",
-         "Data/log_12_02_21.npz",
-         "Data/log_12_02_22.npz",
-         "Data/log_12_02_23.npz",
-         "Data/log_12_02_24.npz",
-         "Data/log_13_02_02.npz",
-         "Data/log_13_02_03.npz" ]
+files = ["ppo_data/log_12_02_20.npz",
+         "ppo_data/log_12_02_21.npz",
+         "ppo_data/log_12_02_22.npz",
+         "ppo_data/log_12_02_23.npz",
+         "ppo_data/log_12_02_24.npz",
+         "ppo_data/log_13_02_02.npz",
+         "ppo_data/log_13_02_03.npz" ]
 
-train_data = data(files)
-batch_size=10
-train_loader = DataLoader(train_data, batch_size=batch_size)
+# train_ppo_data = ppo_data(files)
+# batch_size=10
+# train_loader = ppo_dataLoader(train_ppo_data, batch_size=batch_size)
 
-gain_model = GainTune(batch_size)
+# gain_model = GainTune(batch_size)
 
-opt = torch.optim.Adam(gain_model.parameters(),lr = 0.001)
-criterion = torch.nn.MSELoss()
+# opt = torch.optim.Adam(gain_model.parameters(),lr = 0.001)
+# criterion = torch.nn.MSELoss()
 
-for ep in range(50):
-    run_loss = 0.0
-    print("epoch : ", ep)
-    for i, batch_data in enumerate(train_loader,0):
-        inp = batch_data["ppo"]
-        target = batch_data["pid"]
-        if inp.size()[0]==batch_size:
-            opt.zero_grad()
+# for ep in range(30):
+#     run_loss = 0.0
+#     print("epoch : ", ep)
+#     for i, batch_ppo_data in enumerate(train_loader,0):
+#         inp = batch_ppo_data["ppo"]
+#         target = batch_ppo_data["pid"]
+#         if inp.size()[0]==batch_size:
+#             opt.zero_grad()
 
-            output, h = gain_model(inp)
-
-
-            loss = criterion(output, target)
-            loss.backward()
-            opt.step()
-
-            run_loss+=loss.item()
-
-            if i%100==99:
-                print("\t step : ",i, "\t Loss : ", run_loss/100)
-                run_loss = 0.0
+#             output = gain_model(inp)
 
 
+#             loss = criterion(output, target)
+#             loss.backward()
+#             opt.step()
+
+#             run_loss+=loss.item()
+
+#             if i%100==99:
+#                 print("\t step : ",i, "\t Loss : ", run_loss/100)
+#                 run_loss = 0.0
+
+# torch.save(gain_model.state_dict(),"model.pth")
+
+m1 = GainTune(1)
+m1.load_state_dict(torch.load("model.pth"))
+m1.eval()
+
+files = ["ppo_data/log_13_02_04.npz" ]
+
+test_ppo_data = ppo_data(files)
+batch_size=1
+test_loader = ppo_dataLoader(test_ppo_data, batch_size=batch_size)
+
+op,tr,ips = [],[],[]
+for i, batch_ppo_data in enumerate(test_loader,0):
+    inp = batch_ppo_data["ppo"]
+    target = batch_ppo_data["pid"]
+    if inp.size()[0]==batch_size:
+        # opt.zero_grad()
+        output = m1(inp)
+        out = output[0].squeeze(1).detach().cpu().numpy()
+        target = target[0].squeeze(1).detach().cpu().numpy()
+        inp = inp[:,:,-1][0].detach().cpu().numpy()
+        # print(output[0].squeeze(1),target[0].squeeze(1))
+        op.append(out)
+        tr.append(target)
+        ips.append(inp)
+        # print(out,target)
+
+op,tr,ips = np.array(op),np.array(tr),np.array(ips)
+import matplotlib.pyplot as plt
+
+plt.figure(0)
+ax2 = plt.subplot(3, 1, 1)
+plt.plot( op[:, 1])
+plt.plot(tr[:,1],color='red')
+plt.plot(ips[:,3], label='smooth')
+
+# plt.plot( pose_orientations[:, 2], color='red')
+plt.subplot(3, 1, 2, sharex=ax2)
+plt.plot( op[:, 2])
+plt.plot(tr[:,2]*1.0,color='red')
+plt.plot(ips[:,2], label='smooth')
+
+# plt.plot( pose_orientations[:, 1], color='red')
+plt.subplot(3, 1, 3, sharex=ax2)
+plt.plot( op[:, 3], label='op ppo')
+plt.plot(tr[:,3],color='red', label='pid')
+plt.plot(ips[:,1], label='inp ppo')
+
+# plt.plot( pose_orientations[:, 0], color='red', label='Euler Angle (deg)')
+plt.suptitle('cppo & ang vel cmds')
+plt.legend()
+
+plt.figure(1)
+plt.plot( op[:,0] + 1.0, label = 'op ppo')
+plt.plot( tr[:,0], label = 'pid')
+plt.plot( ips[:,0], label = 'inp ppo')
+plt.legend()
+plt.show()
+
+        # loss = criterion(output, target)
+        # loss.backward()
+        # opt.step()
+
+        # run_loss+=loss.item()
+
+        # if i%100==99:
+        #     print("\t step : ",i, "\t Loss : ", run_loss/100)
+        #     run_loss = 0.0
         # print(output.size())
         # exit()
 
-# a = data1.__getitem__(idx=4)
+# a = ppo_data1.__getitem__(idx=4)
 # import pdb;pdb.set_trace()
 # inp = torch.tensor(np.random.rand(20,4,5))
 # model = GainTune(10)
